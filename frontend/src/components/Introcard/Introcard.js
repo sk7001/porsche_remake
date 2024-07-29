0425794ed9d9a1de9fbeb6f4b66a24d6edc88a6b
sk7001 <sk7001@srmist.edu.in>
2024-07-29 21:39:34 +0530

    finaldeployment

--- frontend/src/components/Introcard/Introcard.js
+++ frontend/src/components/Introcard/Introcard.js
@@ -1,22 +1,10 @@
 import React from 'react'
 import './Introcard.css'
 
-// const Introcard = ({ image, title }) => {
-//     return (
-//             <div className='intro'>
-//                 <img className='video' src={image} alt={title} />
-//                 <div className='content'>
-//                     <h2 className='Card__title'>{title}</h2>
-//                     <button href='#' className='Card__button'>Read More</button>
-//                 </div>
-//             </div>
-//     );
-// };
-
-const Introcard = ({ image, title }) => {
+const Introcard = ({ index, image, title }) => {
     return (
         <div className="intro">
-            <img className="video" src={image} />
+            <img alt={index} className="video" src={image} />
             <div className="introcontent">
                 <label href='#'>{title}</label>
                 <button>Know more</button>
